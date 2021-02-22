3206ccd8ab8f0e4f71ddcc66c09e69befdea0dec
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-02-21 23:33:56 -0500

    DDD

--- module_my_enum.exs
+++ module_my_enum.exs
@@ -6,6 +6,8 @@
 Enum.each([1,2,3], fn(x) -> IO.puts(x) end)
 #2 
 sum = Enum.reduce([1,2,3], 0, fn(x, total) -> x + total end)
+#3 
+Enum.map([1,2,3], fn(x) -> IO.puts(x) end)
 
 defmodule Sample.MyEnum do
   def each([head | tail], function) do
@@ -16,10 +18,15 @@ defmodule Sample.MyEnum do
   def each([], _function), do: nil
 
 
-  def reduce([head | tail], total // 0, function) do
+  def reduce([head | tail], total, function) do
     new_total = function.(head, total)
     reduce(tail, new_total, function)
   end
 
   def reduce([], total, _function), do: total
+
+  def map([head | tail], function) do
+    [function.(head) | map(tail, function)]
+  end
+  def map([], _function), do: []
 end
